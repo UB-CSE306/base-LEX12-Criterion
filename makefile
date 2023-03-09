3b194d3edf642f85e429099ddfafc42fa6b88ad4
Carl Alphonce <alphonce@buffalo.edu>
2023-03-08 23:08:54 -0500

    Updated CUnit -> Criterion

--- makefile
+++ makefile
@@ -1,26 +1,31 @@
-OBJECTS = calculate.o 
-
 # OS identification from: https://stackoverflow.com/questions/714100/os-detecting-makefile
 OS := $(shell uname -s)
 
 ifeq ($(OS), Darwin) 
-  CUNIT_PATH_PREFIX = /usr/local/Cellar/cunit/2.1-3/
-  CUNIT_DIRECTORY = cunit
+  INCLUDE_PATH := /opt/homebrew/Cellar/criterion/2.4.1_1/include
+  LIB_PATH := /opt/homebrew/Cellar/criterion/2.4.1_1/lib
+  CC = gcc-12
 endif
 ifeq ($(OS), Linux) 
-  CUNIT_PATH_PREFIX = /util/CUnit/
-  CUNIT_DIRECTORY = CUnit/
+  INCLUDE_PATH := /util/criterion/include
+  LIB_PATH := /util/criterion/lib/x86_64-linux-gnu
+  CC = gcc
 endif
 
-CC = gcc
-FLAGS = -g -O0 -c -Wall -fprofile-arcs -ftest-coverage 
+FLAGS = -ggdb -O0 -c -Wall -fprofile-arcs -ftest-coverage 
+OBJECTS = calculate.o tests.o
 
 
-calculate.o: calculate.c
+calculate.o: calculate.h calculate.c
 	$(CC) $(FLAGS) calculate.c
 
-tests: $(OBJECTS) tests.c
-	$(CC) -g -O0 -Wall -fprofile-arcs -ftest-coverage -L $(CUNIT_PATH_PREFIX)lib  -I $(CUNIT_PATH_PREFIX)include/$(CUNIT_DIRECTORY) $(OBJECTS) tests.c -o tests -lcunit -lgcov 
+tests.o: calculate.h tests.c 
+	$(CC) -c $(CFLAGS) -I $(INCLUDE_PATH) tests.c
 
+tests:  calculate.o tests.o
+	$(CC) $(CFLAGS) -L $(LIB_PATH) -I $(INCLUDE_PATH) -o tests $(OBJECTS) -lcriterion -lgcov
+
+
+.PHONY: clean
 clean:
-	rm -rf *~ *.o *.dSYM a.out runner tests *.xml *.gc??
+	rm -rf *~ $(OBJECTS) *.dSYM tests *.gc??
--- tests.c
+++ tests.c
@@ -1,50 +1,12 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include "CUnit.h"
-#include "Automated.h"
+#include <criterion/criterion.h>
 #include "calculate.h"
 
 
-/* Runs a test input that *should* pass and produce an int result
- */
-void test01(void) {
-  int expected = 100;
-  int actual = calculate(13000,0,1);
-  CU_ASSERT_EQUAL( expected , actual );
+void runtest(int income, int minors, int adults, int expected) {
+    int actual = calculate(income, minors, adults);
+    cr_assert_eq(actual, expected, "calculate(%d,%d,%d) should be %d, but was %d.\n", income, minors, adults, expected, actual);
 }
 
-/* The main() function for setting up and running the tests.
- * Returns a CUE_SUCCESS on successful running, another
- * CUnit error code on failure.
- */
-int main()
-{
-   CU_pSuite Suite = NULL;
 
-   /* initialize the CUnit test registry */
-   if (CUE_SUCCESS != CU_initialize_registry()) { return CU_get_error(); }
-
-   /* add a suite to the registry */
-   Suite = CU_add_suite("Suite_of_tests_with_valid_inputs", NULL, NULL);
-   if (NULL == Suite) {
-      CU_cleanup_registry();
-      return CU_get_error();
-   }
-
-   /* add the tests to Suite */
-   if (
-          (NULL == CU_add_test(Suite, "13000/0/1", test01))
-      )
-   {
-      CU_cleanup_registry();
-      return CU_get_error();
-   }
-
-   /* Run all tests using automated interface, with output to 'test-Results.xml' */
-   CU_set_output_filename("test");
-   CU_automated_run_tests();
-
-   CU_cleanup_registry();
-   return CU_get_error();
-}
+// Tests
+Test(lex12, test01) { runtest(13000, 0, 1, 100); }
